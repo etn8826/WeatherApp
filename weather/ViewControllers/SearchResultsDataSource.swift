36122e613f6932a3bc92701fcc551005ecd7264e
etn8826 <etn8826@gmail.com>
2025-10-19 16:46:46 -0400

    add an independant search table

---
+++ weather/ViewControllers/SearchResultsDataSource.swift
@@ -0,0 +1,50 @@
+//
+//  SearchResultsDataSource.swift
+//  weather
+//
+//  Created by Einstein Nguyen on 10/19/25.
+//
+
+import UIKit
+import MapKit
+
+class SearchResultsDataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
+    private weak var tableView: UITableView?
+    private var results: [MKLocalSearchCompletion] = []
+    var didSelect: ((MKLocalSearchCompletion) -> Void)?
+
+    init(tableView: UITableView) {
+        self.tableView = tableView
+        super.init()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    func update(_ results: [MKLocalSearchCompletion]) {
+        self.results = results
+        tableView?.reloadData()
+    }
+
+    // MARK: UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let id = "SearchResultCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: id) ??
+            UITableViewCell(style: .subtitle, reuseIdentifier: id)
+        let item = results[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.detailTextLabel?.text = item.subtitle
+        cell.selectionStyle = .none
+        return cell
+    }
+
+    // MARK: UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = results[indexPath.row]
+        didSelect?(item)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
